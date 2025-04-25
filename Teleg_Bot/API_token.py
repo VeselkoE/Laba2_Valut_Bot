6bd8c5b9786b527fbde87bd7997f61e1d6f98c98
VeselkoEuser.email <email@example.ru>
2025-04-25 18:44:06 +0300

    new

--- Teleg_Bot/API_token.py
+++ Teleg_Bot/API_token.py
@@ -1,9 +1,28 @@
 """
 Сдесь указан токен и доступная валюта
 """
-MY_TOKEN = '7698137597:AAEC0Hl3EiYt4j1CiSd_kSnJVcJQqcIIUkk'
-all_key = {
-    'Рубль': 'RUB',
-    'Доллар': 'USD',
-    'Евро': 'EUR',
-}
+# MY_TOKEN = '7698137597:AAEC0Hl3EiYt4j1CiSd_kSnJVcJQqcIIUkk'
+# all_key = {
+#     'Рубль': 'RUB',
+#     'Доллар': 'USD',
+#     'Евро': 'EUR',
+# }
+#import chardet
+def load_config(filename):
+    config = {}
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            key, value = line.strip().split('=')
+            config[key] = value
+    return config
+
+# Загрузка данных из файла
+config = load_config('API_TOKEN.txt')
+
+# Получение токена и валют
+MY_TOKEN = config.get('MY_TOKEN')
+all_key = {key: value for key, value in config.items() if key != 'MY_TOKEN'}
+
+# Проверка
+print("Токен:", MY_TOKEN)
+print("Словарь валют:", all_key)
